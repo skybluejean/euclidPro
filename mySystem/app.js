5ff7fdcf1249b55296ade199b8e32557ee7a7276
jin <ellenjin51@gmail.com>
2023-12-08 01:51:59 +0900

    초기 썸네일게시판 기틀 구현

---
+++ mySystem/app.js
@@ -0,0 +1,46 @@
+// app.js
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    // 루트 경로에 대한 요청 처리
+    if (req.url === '/' || req.url === '/index.html') {
+        const filePath = path.join(__dirname, 'public', 'index.html');
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    }
+
+    // /api/posts 경로에 대한 요청 처리
+    else if (req.url === '/api/posts') {
+        const filePath = path.join(__dirname, 'data', 'posts.json');
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(data);
+            }
+        });
+    }
+
+    // 지원하지 않는 경로에 대한 처리
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+const port = 3050;
+server.listen(port, () => {
+    console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+});
